ad9e74f02b1e0e0c023ef4564073125ebaf5279c
Darklight437 <darklight437@gmail.com>
2017-10-23 14:49:54 +1100

    Continuing UI mockup For play state
    Mostly light UI mockup

---
+++ Assets/David/scripts/NextTurnButton.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class NextTurnButton : MonoBehaviour
+{
+    //David
+    public Color Red;
+    public Color Blue;
+    private GameObject ColorChangers;
+    private Image Panel;
+    void Start()
+    {
+        ColorChangers = GameObject.FindGameObjectWithTag("RedVSBlue");
+        //Put an if on whose turn to determine default UI color
+        Panel = ColorChangers.GetComponent<Image>();
+        Panel.color = Red;
+    }
+
+   public void Click()
+    {
+        //toggle bool turn
+        
+        if(Panel.color == Red)
+        {
+            Panel.color = Blue;
+        }
+        else
+        {
+            Panel.color = Red;
+        }
+        
+    }
+	
+}
