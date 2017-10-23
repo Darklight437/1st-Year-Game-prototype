0146ff2cd957c40ef44d3ffe7c1e79da8915bc6a
Darklight437 <darklight437@gmail.com>
2017-10-24 10:03:39 +1100

    Worldspace canvas changes

---
+++ Assets/David/scripts/WorldspaceManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldspaceManager : MonoBehaviour {
+    //David
+
+    public GameObject MoveButton;
+    public GameObject AttButton;
+    public GameObject SpcButton;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
