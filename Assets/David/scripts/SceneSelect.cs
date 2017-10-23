8be6548d9444d65150b255d49e5f63feec75a800
Darklight437 <darklight437@gmail.com>
2017-10-23 12:18:54 +1100

    Menu mockup scene & scene changer script

---
+++ Assets/David/scripts/SceneSelect.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSelect : MonoBehaviour {
+    //David
+
+    public Scene ChangeTo;
+    public enum eChangeSceneTo { MENU, PLAY };
+    public eChangeSceneTo NextScene;
+    // Use this for initialization
+    void Start ()
+    {
+        ChangeTo = SceneManager.GetSceneByBuildIndex((int)NextScene);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+	}
+
+    public void SceneTest()
+    {
+        if (SceneManager.GetActiveScene().buildIndex == (int)eChangeSceneTo.MENU)
+        {
+            SceneManager.SetActiveScene(ChangeTo);
+        }
+    }
+}
