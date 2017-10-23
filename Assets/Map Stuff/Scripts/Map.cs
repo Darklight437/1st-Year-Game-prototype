b26f836079590491dbab8dec9f180f6315f27e1e
Unknown <callum.dunstone@gmail.com>
2017-10-24 09:15:06 +1100

    Added in map
    ported in the map editor, map editor is currently Set up in the map Editor Stuff scene. all the current tiles are place holders. inorder to use the map editor select the game object called map editor, you can set the maps length and width. click generate base map to creat a bland map then click toggle paint mode to enter the paint mode from there more buttons should appear to select diffrent tiles to pain alternativly you can right click on a tile to cycle through the diffrent tile types

---
+++ Assets/Map Stuff/Scripts/Map.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+* class Map
+* inherits MonoBehaviour
+* 
+* this class holds all the map tiles parented to its game object
+* 
+* author: Callum Dunstone, Academy of Interactive Entertainment, 2017
+*/
+public class Map : MonoBehaviour
+{
+    //list of all the tiles
+    public List<Tiles> mapTiles;
+
+    /*
+    * Start
+    * public void function
+    * 
+    * this function is called at the start of the scripts life
+    * in play mode
+    * 
+    * @returns nothing
+    */
+    void Start ()
+    {
+        SetTileEdges();
+    }
+
+    /*
+    * SetTileEdges
+    * public void function
+    * 
+    * this function goes through all the tiles finding there
+    * adjacents on North, South, East and west and connects them up for pathfinding
+    * purposes
+    * 
+    * @returns nothing
+    */
+    private void SetTileEdges()
+    {
+        //goes through each of the nodes
+        foreach (Tiles tileA in mapTiles)
+        {
+            tileA.tileEdges = new List<Tiles>();
+            foreach (Tiles tileB in mapTiles)
+            {
+                //gets the offset of the x and z to determin if the tile is next to the one we are looking at
+                float offsetX = tileA.transform.position.x - tileB.transform.position.x;
+                float offsetZ = tileA.transform.position.z - tileB.transform.position.z;
+
+                //true if tile to the East
+                if (offsetX == 1 && offsetZ == 0)
+                {
+                    tileA.tileEdges.Add(tileB);
+                }
+
+                //true if tile to the South
+                if (offsetX == 0 && offsetZ == -1)
+                {
+                    tileA.tileEdges.Add(tileB);
+                }
+
+                //true if tile to the West
+                if (offsetX == -1 && offsetZ == 0)
+                {
+                    tileA.tileEdges.Add(tileB);
+                }
+
+                //true if tile to the North
+                if (offsetX == 0 && offsetZ == 1)
+                {
+                    tileA.tileEdges.Add(tileB);
+                }
+            }
+        }
+    }
+}
