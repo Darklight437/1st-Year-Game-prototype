84dc05d5b29d752dbf1db88423f693595416f2a7
Unknown <callum.dunstone@gmail.com>
2017-10-24 13:42:02 +1100

    Added in A*

---
+++ Assets/Map/Scripts/A star/AstarTest.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AstarTest : MonoBehaviour
+{
+    public Tiles start = null;
+    public Tiles end = null;
+
+    public List<Tiles> path;
+	
+	void Update ()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            //sets out a raycast from the mouses position
+            if (Physics.Raycast(ray, out hit))
+            {
+                //if we hit a tile we enter paint mode
+                if (hit.transform.tag == "Tile")
+                {
+                    if (start == null)
+                    {
+                        start = hit.transform.GetComponent<Tiles>();
+                    }
+                    else if (end == null)
+                    {
+                        end = hit.transform.GetComponent<Tiles>();
+                    }
+                    else
+                    {
+                        start = end;
+                        end = hit.transform.GetComponent<Tiles>();
+                    }
+
+                }
+            }
+        }
+
+        if (Input.GetKeyDown("space"))
+        {
+            foreach (Tiles tile in path)
+            {
+                tile.tileType = eTileType.NORMAL;
+                tile.GenerateRandomTileVariant();
+            }
+            path = AStar.g_AStarInstance.GetAStarPath(start, end);
+            foreach (Tiles tile in path)
+            {
+                tile.tileType = eTileType.PATH;
+                tile.GenerateRandomTileVariant();
+            }
+        }
+    }
+}
