3669715c25847e3d8f099e2501bde7002673d828
Darklight437 <darklight437@gmail.com>
2017-10-30 09:54:24 +1100

    Unit highlight testing

---
+++ Assets/David/scripts/GlowTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlowTest : MonoBehaviour {
+
+    public GameObject testModel;
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+
+    public void GlowLikeABastard()
+    {
+        testModel.GetComponent<Renderer>().material.shader = Shader.Find("Self-Illumin/Outlined Diffuse");
+    }
+}
