4a5750d4e905a3bb96d661c2ed22e38e0db9e32d
Brad <nutty.yotuber@gmail.com>
2017-10-31 13:35:51 +1100

    damage tiles

--- Assets/Brad/Scripts/Command/SpreadAttackCommand.cs
+++ Assets/Brad/Scripts/Command/SpreadAttackCommand.cs
@@ -62,7 +62,7 @@ public class SpreadAttackCommand : UnitCommand
         int maxDistance = Mathf.CeilToInt(attackRadius);
 
         //get the surrounding tiles, considering obstacles
-        List<Tiles> area = GetArea.GetAreaOfMoveable(endTile, maxDistance);
+        List<Tiles> area = GetArea.GetAreaOfAttack(endTile, maxDistance);
 
         //get the size of the area tiles list
         int areaSize = area.Count;
